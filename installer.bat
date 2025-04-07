64cfdf5057271308485129fae8fe1603d9b5a24e
ShadowCatlol <blxrewards@gmail.com>
2025-04-07 17:26:56 -0400

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,31 +1,5 @@
 @echo off
 setlocal EnableDelayedExpansion
-
-set "procname=cmd.exe"
-for /f %%A in ('tasklist /fi "imagename eq %procname%" ^| find /c /i "%procname%"') do set "count_before=%%A"
-
-set "vbsfile=%temp%\elevate.vbs"
-> "%vbsfile%" echo Set UAC = CreateObject^("Shell.Application"^)
->>"%vbsfile%" echo UAC.ShellExecute "%~f0", "", "", "runas", 1
-cscript //nologo "%vbsfile%" >nul
-del "%vbsfile%"
-
-:: Wait a bit for elevated proc to start
-timeout /t 5 >nul
-
-:: Count processes after elevation attempt
-for /f %%A in ('tasklist /fi "imagename eq %procname%" ^| find /c /i "%procname%"') do set "count_after=%%A"
-
-echo Processes after: %count_after%
-
-:: Compare counts
-if %count_after% GTR %count_before% (
-    echo Elevated instance detected. Exiting this one.
-    exit /b
-) else (
-    echo Admin denied...
-)
-
 color 6
 echo [LOG] Installing Drivers
 
